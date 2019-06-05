758d8c66758d99c15ecdf5aaf179e6897bdd8f7e
Juho Niemistö <juho.niemisto@iki.fi>
2019-06-05 20:11:14 +0300

    Add Storybook and basic story for togglable text.

---
+++ src/components/TogglableText.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+  toggled: boolean;
+  text: string;
+  onClick?: () => void;
+}
+
+export default class TogglableText extends React.Component<Props> {
+  public render() {
+    return "Hoi";
+  }
+};
