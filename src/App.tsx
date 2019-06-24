3584c5356860657423c77935849b071206e29c90
Juho Niemistö <juho.niemisto@iki.fi>
2019-06-24 22:30:48 +0300

    Implement basic routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import MuscleMap from './MuscleMap';
 
 import './App.css';
 
+const Home: React.FC = () => {
+  return <div>Etusivu</div>;
+}
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <MuscleMap/>
-      Hoi
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Etusivu</Link></li>
+            <li><Link to="/muscles">Lihakset</Link></li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/muscles" component={MuscleMap} />
+      </div>
+    </Router>
   );
 }
 
